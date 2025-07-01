eaec03b6e2b5814f32b5b29202dd8d434821adcb
George Ndalu <ndalugeorge2001@gmail.com>
2025-07-01 07:24:03 +0300

    Resolve module not found error on vercel

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3Zw6x3h5MQZhjWLFXZIy5JVbjVmUsl8tmb45XLJcTrc=\"\n}"
